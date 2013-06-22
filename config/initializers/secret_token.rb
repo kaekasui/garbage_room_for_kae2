9a22059c40a554789b58ea7ab92ebb62d0f44ed2
@kae_kasui <kaekasui_github@icloud.com>
2013-06-22 19:38:34 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RoomForKae2::Application.config.secret_key_base = '186081d03c92bf8def8f450e8cc01aeb89e5ed51479496f0cd542a5c148d55b1d6f964428305b8a2b4f8fb546e6097ad4ea267255dd006034a32f9c5acbc8bd0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RoomForKae2::Application.config.session_store :cookie_store, key: '_room_for_kae2_session'
