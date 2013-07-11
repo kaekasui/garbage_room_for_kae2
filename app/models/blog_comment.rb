e0917329f39880100da59568677e208d36bdb909
@kae_kasui <kaekasui_github@icloud.com>
2013-07-12 03:51:01 +0900

    refs #181 : ブログとコメントの関係見直し

--- app/models/blog_comment.rb
+++ app/models/blog_comment.rb
@@ -1,2 +1,15 @@
 class BlogComment < ActiveRecord::Base
+
+  belongs_to :blog
+  before_save :set_hostname
+
+  private
+  def set_hostname
+    self.host = hostname(self.ip)
+  end
+
+  def hostname(ip)
+     hostname = Socket.getnameinfo([nil, nil, nil, ip])
+     hostname[0]
+  end
 end
