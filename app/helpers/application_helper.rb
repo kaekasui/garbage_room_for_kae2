9eaa8dd24385601739eb694876568e9bbb638d68
@kae_kasui <kaekasui_github@icloud.com>
2013-07-13 23:23:22 +0900

    refs #181 : ブログコメントの必須項目に*を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,10 @@ module ApplicationHelper
   def to_br(text)
     text.gsub(/\r\n|\r|\n/, "<br />")
   end
+
+  # 入力必須の項目であった場合に「*」を表示する
+  def asterisk(object, attribute)
+    mark = raw "<span class='asterisk'>*</span>"
+    mark if object.class.validators_on(attribute).map(&:class).include? ActiveRecord::Validations::PresenceValidator
+  end
 end
