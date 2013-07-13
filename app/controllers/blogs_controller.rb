4b3909e2a33f0784f9cad1fa994e1037c3f978ee
@kae_kasui <kaekasui_github@icloud.com>
2013-07-14 02:31:54 +0900

    refs #181 : 不要なメソッドを削除

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,4 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show]
   before_action :set_blogs_data, only: [:index, :show]
 
   # GET /blogs
@@ -16,69 +15,15 @@ class BlogsController < ApplicationController
     @blog_comments = @blog.blog_comments
   end
 
-  # GET /blogs/new
-  def new
-    @blog = Blog.new
-  end
-
-  # GET /blogs/1/edit
-  def edit
-  end
-
-  # POST /blogs
-  # POST /blogs.json
-  def create
-    @blog = Blog.new(blog_params)
-
-    respond_to do |format|
-      if @blog.save
-        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @blog }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /blogs/1
-  # PATCH/PUT /blogs/1.json
-  def update
-    respond_to do |format|
-      if @blog.update(blog_params)
-        format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /blogs/1
-  # DELETE /blogs/1.json
-  def destroy
-    @blog.destroy
-    respond_to do |format|
-      format.html { redirect_to blogs_url }
-      format.json { head :no_content }
-    end
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_blog
-      @blog = Blog.find(params[:id])
-    end
 
-    def set_blogs_data
-      @recent_blogs = Blog.recent_blogs
-      @blog_links = BlogLink.all
-      @recent_blog_comments = BlogComment.recent_blog_comments
-    end
+  def set_blogs_data
+    @recent_blogs = Blog.recent_blogs
+    @blog_links = BlogLink.all
+    @recent_blog_comments = BlogComment.recent_blog_comments
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def blog_params
-      params.require(:blog).permit(:title, :blog_category_code, :contents1, :contents2, :draft, :deleted_at)
-    end
+  def blog_params
+    params.require(:blog).permit(:title, :contents1, :contents2)
+  end
 end
