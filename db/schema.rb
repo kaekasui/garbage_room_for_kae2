71de8c399882f5bb3719bae8ac3ff2dee90f4bf6
@kae_kasui <kaekasui_github@icloud.com>
2013-07-14 02:32:31 +0900

    refs #221 : 'rake db:migrate'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130711183812) do
+ActiveRecord::Schema.define(version: 20130713172018) do
 
   create_table "blog_comments", force: true do |t|
     t.integer  "blog_id"
@@ -64,6 +64,16 @@ ActiveRecord::Schema.define(version: 20130711183812) do
     t.string   "host"
     t.integer  "user_id"
     t.boolean  "check"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "screen_name"
+    t.string   "image"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
